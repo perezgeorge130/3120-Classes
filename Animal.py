c9af2d6e734b8e1e950e14400d709180475dc8cd
perezgeorge130 <perezgeorge130@gmail.com>
2025-02-24 20:34:18 -0500

    George code

--- Animal.py
+++ Animal.py
@@ -1,8 +1,17 @@
 class Animal:
-    def __init__(self, name):
+    def __init__(self, name, kind, age=1, weight=5):
         self.__name = name 
-        print("hello, I am", self.__name)
+        self.__age = age
+        self.__weight = weight
+        self.__kind = kind
+        print(f"hello, I am {self.__name} and I am a {self.__kind}")
 
-    def talk(self):
-        print("hi")
+    def born(self, name, kind):
+        print(f"Hello world!, my name is {self.__name}. I am a {self.__kind} and I am {self.__age}")
+    
+    def weight(self):
+        print(f'My current weight is {self.__weight}')
 
+    def gain(self, weight):
+        self.__weight+=weight
+        print(f'I gained {weight} pounds. Current weight: {self.__weight}')
