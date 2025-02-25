0a5c484fb9927b737fb0dc3a0486227e0a2b1c62
preeti2grg <preeti.gurung1@baruchmail.cuny.edu>
2025-02-24 20:06:31 -0500

    my code

--- Animal.py
+++ Animal.py
@@ -1,8 +1,19 @@
 class Animal:
-    def __init__(self, name):
-        self.__name = name 
+
+    def __init__(self, name, animal, color): 
+        self.__name = name
+        self.__animal = animal
+        self.color = color
         print("hello, I am", self.__name)
 
     def talk(self):
         print("hi")
+
+    def animal_type(self, animal):  
+        print (f"I am a {animal}")
+    
+
+    def animal_color(self, color):
+        print (f"Animal color:{color}")
+
 
